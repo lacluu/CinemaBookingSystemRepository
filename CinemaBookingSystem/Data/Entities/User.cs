5333294912370df20c6801bbcdeca3dccc962716
luu lac <luulac2012@gmail.com>
2018-05-18 12:27:59 +0700

    Reupload project

---
+++ CinemaBookingSystem/Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Data.Entities
+{
+    public class User
+    {
+        private String Account { get; set; }
+        private int Id { get; set; }
+        private String UrlAvatar { get; set; }
+
+        public User(String account, int id, String urlAvatar)
+        {
+            this.Account = account;
+            this.Id = id;
+            this.UrlAvatar = urlAvatar;
+        }
+    }
+}
